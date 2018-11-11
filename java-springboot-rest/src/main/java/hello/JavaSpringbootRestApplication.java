77c875e939bd867e7cb5688feff93d52e16c1dc2
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-11-11 15:48:01 +0100

    Initial commit

---
+++ java-springboot-rest/src/main/java/hello/JavaSpringbootRestApplication.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author laeti
+ *
+ */
+@SpringBootApplication
+public class JavaSpringbootRestApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootRestApplication.class, args);
+	}
+}
